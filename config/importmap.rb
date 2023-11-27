c9f1d85da09f10630441d080b6530fb1f1d086f9
Muhammad Ahsan <muhammad.ahsan@healthwire.pk>
2023-11-27 22:42:43 +0500

    Gem Importmap

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,8 +1,16 @@
-# Pin npm packages by running ./bin/importmap
+# config/importmap.rb
+# Use Action Cable channels (remember to import "@rails/actionable" in your application.js)
+# pin "@rails/actioncable", to: "actioncable.esm.js"
+# pin_all_from "app/javascript/channels", under: "channels"
 
-pin "application", preload: true
-pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+# Use direct uploads for Active Storage (remember to import "@rails/activestorage" in your application.js)
+# pin "@rails/activestorage", to: "activestorage.esm.js"
+
+# Use node modules from a JavaScript CDN by running ./bin/importmap
+
+pin "application"
+pin "@hotwired/stimulus", to: "stimulus.js"
+pin "@hotwired/stimulus-importmap-autoloader", to: "stimulus-importmap-autoloader.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-pin "sortablejs", to: "https://cdn.jsdelivr.net/npm/sortablejs@1.15.0/modular/sortable.esm.js"
+pin "@hotwired/turbo-rails", to: "turbo.js"
+pin "sortablejs", to: "https://cdn.jsdelivr.net/npm/sortablejs@1.14.0/modular/sortable.esm.js"
